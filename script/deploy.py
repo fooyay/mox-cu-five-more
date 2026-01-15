1f816c86cf4069078a0a01d5b9c99a5c4960b89e
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-15 17:48:38 -0500

    have the deployer make a factory contract

---
+++ script/deploy.py
@@ -0,0 +1,24 @@
+from src import favorites, favorites_factory  # type: ignore
+from moccasin.boa_tools import VyperContract  # type: ignore
+
+
+def deploy_favorites() -> VyperContract:
+    favorites_contract: VyperContract = favorites.deploy()
+    print(f"Favorites contract deployed at: {favorites_contract.address}")
+    return favorites_contract
+
+
+def deploy_factory(favorites_contract: VyperContract) -> None:
+    factory_contract: VyperContract = favorites_factory.deploy(favorites_contract.address)
+    factory_contract.create_favorites_contract()
+
+    new_favorites_address: str = factory_contract.list_of_favorite_contracts(0)
+    new_favorites_contract: VyperContract = favorites.at(new_favorites_address)
+    new_favorites_contract.store(77)
+    retrieved_value: int = new_favorites_contract.retrieve()
+    print(f"Retrieved value from new Favorites contract: {retrieved_value}")
+
+
+def moccasin_main():
+    favorites_contract = deploy_favorites()
+    deploy_factory(favorites_contract)
--- src/favorites_factory.vy
+++ src/favorites_factory.vy
@@ -15,3 +15,9 @@ def __init__(original_favorite_contract: address):
 def create_favorites_contract():
     new_favorites_contract: address = create_copy_of(self.original_favorite_contract)
     self.list_of_favorite_contracts.append(new_favorites_contract)
+
+@external
+def store_from_factory(favorites_index: uint256, new_number: uint256):
+    favorites_address: address = self.list_of_favorite_contracts[favorites_index]
+    favorites_contract: i_favorites = i_favorites(favorites_address)
+    extcall favorites_contract.store(new_number)
